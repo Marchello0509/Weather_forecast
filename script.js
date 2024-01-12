55411dcbfc7aaa642b9e97eb8095737a611d7464
Marchello <129538566+Marchello0509@users.noreply.github.com>
2024-01-12 19:35:33 +0200

    Sv1

---
+++ script.js
@@ -0,0 +1,21 @@
+const html = {
+    page404: "./404/index.html",
+    Cloudy: "./Cloudy/index.html",
+    Cloudyn: "./Cloudy night /index.html",
+    Fog: "./Fog4/index.html",
+    Hi: "./Hi!/index.html",
+    Lightning: "./Lightning/index.html",
+    Night: "./Night/index.html",
+    Rainy: "./Rainy/index.html",
+    Snow: "./Snowy/index.html",
+    Sunny: "./Sunnyy/index.html",
+    Wetsnow: "./Wet snow/index.html",
+    Windy: "./Windy/index.html",
+}
+
+const siteEl = {
+    btnGo: document.querySelector('#btnGo'),
+    inpCity: document.querySelector('#inpCity')
+}
+
+console.log(siteEl)
