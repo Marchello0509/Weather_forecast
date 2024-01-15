63761ddd50fce6a3b2889d359b2d61ec77063a70
Marchello <129538566+Marchello0509@users.noreply.github.com>
2024-01-15 19:51:03 +0200

    Sv4

--- script.js
+++ script.js
@@ -15,7 +15,23 @@ const html = {
 
 const siteEl = {
     btnGo: document.querySelector('#btnGo'),
-    inpCity: document.querySelector('#inpCity')
+    inpCity: document.querySelector('#inpCity'),
+    block2: document.querySelector('#block2'),
 }
 
-console.log(siteEl)
+const apiKey = "440f7425b1154fcdb51171503241301"
+const callApi = "http://api.weatherapi.com/v1/current.json"
+let input = ""
+
+siteEl.btnGo.addEventListener('click', () => {
+    siteEl.block2.innerHTML = ''
+    axios.get(`${callApi}?key=${apiKey}&q=${input}`)
+        .then(res => {
+            console.log(res.data)
+            
+        })
+})
+
+siteEl.inpCity.addEventListener('input', (e) => {
+    input = e.target.value
+})
